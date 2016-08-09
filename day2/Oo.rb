e4e541728a8165c879129d2b3a857bafe6d1a2aa
L.Carquet <laetiticarquet@gmail.com>
2016-08-09 15:48:23 +0200

    non finished activity or oriented object

---
+++ day2/Oo.rb
@@ -0,0 +1,59 @@
+class Car 
+	def initialize noise
+		@noise = noise
+		@list_of_cities = load_cities
+	end
+	def make_noise
+		puts @noise
+	end
+	def self.speed_control
+		puts "What is your current speed?"
+		user_answer = gets.chomp.to_i
+		if user_answer > 100
+			puts "You should slow down !"
+		end
+	end
+
+#	def visit_city(city)
+#		IO.read("cities.txt")
+#	end
+
+#	def record_visit
+#		@list_of_cities<<city
+#		IO.read()
+#	end
+
+#	def cities
+#		@list_of_cities = load_cities
+#	end
+#This method does not answer the question of the exercise. It prints the cities but do not return the array.
+#	def show_visit
+#		@list_of_cities.each do |city|
+#			puts "The cities you visited are: #{city}"
+#		end
+#	end
+end
+
+class RacingCar < Car
+	def initialize
+		@noise = "brooooom"
+	end
+end	
+
+
+class Person 
+	attr_reader :name
+	def initialize(name, age)
+	@name = name
+	@age = age
+	end
+end
+
+
+my_car = Car.new "pouet pouet"
+# '''other_car = Car.new "honk honk"
+# my_car.make_noise
+# other_car.make_noise'''
+#my_car.speed_control
+my_car.record_visit("Madrid")
+my_car.cities
